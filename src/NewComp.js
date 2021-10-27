69e650c44648c09b2201a5507c1c3393aa361f30
shubhloni <lonishubham@gmail.com>
2021-10-27 13:51:58 +0530

    1 file modified

--- src/NewComp.js
+++ src/NewComp.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NewComp = (props) =>{
-  return(
-    <p> {props.data} </p>
-  )
-}
+const NewComp = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementByone = () => {
+    setCount((count)=> count + 1);
+  };
+
+  const incrementByFour = () => {
+    for (let i = 0; i < 4; i++) {
+      incrementByone();
+    }
+  };
+
+  return (
+    <div className="App">
+      <button onClick={incrementByone}> incrementByone </button>
+      <button onClick={incrementByFour}> incrementByFour </button>
+      <div>{count}</div>
+    </div>
+  );
+};
 
 export default NewComp;
